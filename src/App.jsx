6644b77a4f001b35bb304391d74095895b3dd89c
Hamza Esadik <hamza.esadik.m@gmail.com>
2025-03-12 10:16:40 +0000

    section6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 
-import './App.css'
-import { Button } from './components/ui/button'
-
 function App() {
 
 
   return (
     <>
-      <h1 className='text-2xl text-center'>Hello There!</h1>
-      <Button>Click me</Button>
 
     </>
   )
